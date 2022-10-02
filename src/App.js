182772996a73373b6fc857173d42fdcc87134c4f
eduJSilva <edusilvaubp@gmail.com>
2022-10-01 23:40:59 -0300

    drummachine 2do commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+//import React, {useEffect}from 'react';
 import logo from './logo.svg';
 import { Counter} from './features/counter/Counter';
 import './App.css';
-import {useEffect} from 'react';
 
 let counter = new Counter();
 
 function App() {
-  useEffect(() =>{
-    document.addEventListener('keydown', counter.detectKeyDown, true)}, []);
+  /*
+   let detectKeyDown = (e) => {
+      let letters = counter.keys;
+      for (let letra of letters) {
+        if (letra === e.key.toUpperCase()) {
+          console.log(letra);
+          let audio = new Audio(counter.state.padSounds[letra]);
+          return  audio.play()
+        }
+        else {}
+      }
+    };
 
-   
+    useEffect(()=>{document.addEventListener("keydown", detectKeyDown, true)}, [])
+   */
 
   return (
-    <div className="App">    
+    <div className="App" onKeyPress={counter.handleKeyPress}>    
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Drum Machine</h1> 
-        <Counter/>
+        <Counter />
       </header>
     </div>
   );
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -2,58 +2,98 @@ import React from "react";
 //import styles from './Counter.module.css';
 
 export class Counter extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = {     
-    }    
-    this.keys=this.arrayKeys(this.props.padSounds)
+    this.state = {
+      padSounds: {
+        Q: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Yamaha%20DX100/22[kb]dx100-Pink-noise-C3a.wav.mp3",
+        W: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Yamaha%20DX100/65[kb]dx100-TomToms-C3.wav.mp3",
+        E: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Roland%20SH09/44[kb]stompbass1.wav.mp3",
+        A: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Pearl%20SC-40%20Kicks/65[kb]SC40BD028.wav.mp3",
+        S: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Pearl%20SC-40%20Kicks/35[kb]SC40BD07.wav.mp3",
+        D: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/34[kb]linn-BDRUM13.wav.mp3",
+        Z: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/15[kb]linn-Clap-1.wav.mp3",
+        X: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/14[kb]linn-COWB-v2.wav.mp3",
+        C: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/122[kb]linn-RIDE-v2.wav.mp3",
+      },
+      input: "",
+    };
+    this.keys = this.arrayKeys(this.state.padSounds);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  arrayKeys(object){
-    return Object.keys(object)
-  }
-
-  detectKeyDown = (e) =>{
-   let letters= this.keys
-    for(let letra of letters) {
-      console.log(letra)
-     if (letra.id === e.key) {
-       let audio = new Audio(this.state.padSounds[letra]);
-       return audio.play();
-     }
-    } 
-   }
-
-
-
-render() {
- 
-
-  const drumpad = this.keys.map((one)=>{  
-     let idx = React.createRef()
-    function sound() {
-      idx.current.play()
-    }
   
-    return  <span>
-    <button   onClick={sound} id={one+this.keys.indexOf(one)} 
-    style={{backgroundColor: 'whitegrey', margin:5, width: 50, height: 50,
-                border: '4px solid black'}} className='drum-pad'>{one}<audio id={one} ref={idx} className='clip'     
-                       src={this.state.padSounds[one]}>
-                       </audio> 
-                       </button>          
-    </span>
-   });
+  arrayKeys(object) {
+    return Object.keys(object);
+  }
 
-  return (
-    <div className='container-fluid' id="drum-machine">
-    <div id="display">
-    <h2>{}</h2>
-    {drumpad}
-    </div>
-    </div>
-  )
-}
+   handleChange(e) {
+    this.setState({
+      input: e
+    });
 
+  }
+
+  handleKeyPress = (event) => {
+    let letters = this.keys
+    for (let letra of letters) {
+      if(event.key.toUpperCase() === letra){
+        console.log(letra+' press here! ')
+        let audio = new Audio(this.state.padSounds[letra]);
+       let go=()=>this.handleChange(letra.toString())
+       console.log(go)
+       
+        return audio.play()
+      }
+      else {}
+  }
+  }
+
+  render() {
+    const drumpad = this.keys.map((one) => {
+      let idx = React.createRef();
+
+      function sound() {
+       sound.currentTime = 0;
+        idx.current.play();
+      }
+
+      return (
+        <span onClick={() => this.handleChange(one.toString())}>
+          <button
+            onClick={sound} 
+            id={one + this.keys.indexOf(one)}
+            style={{
+              backgroundColor: "whitegrey",
+              margin: 5,
+              width: 50,
+              height: 50,
+              border: "4px solid black",
+            }}
+            className="drum-pad"
+          >
+            {one}
+            <audio
+              id={one}
+              ref={idx}
+              className="clip"
+              src={this.state.padSounds[one]}
+            ></audio>
+          </button>
+        </span>
+      );
+    });
+
+
+    return (
+      <div className="container-fluid" id="drum-machine">
+        <div id="display">
+          <h2>{
+           this.state.input
+          }</h2>
+          {drumpad}
+        </div>
+      </div>
+    );
+  }
 }
