6ea6117d6c34491a6e0370f3244d630436db7ff1
Eduardo J Silva <silvaeduardojavier@hotmail.com>
2022-10-02 16:43:14 -0300

    Diseño con React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-//import React, {useEffect}from 'react';
-import logo from './logo.svg';
-import { Counter} from './features/counter/Counter';
+import logo from './logo.svg';  
 import './App.css';
-
-let counter = new Counter();
+import { Counters } from './features/counter/Counter';
 
 function App() {
-  /*
-   let detectKeyDown = (e) => {
-      let letters = counter.keys;
-      for (let letra of letters) {
-        if (letra === e.key.toUpperCase()) {
-          console.log(letra);
-          let audio = new Audio(counter.state.padSounds[letra]);
-          return  audio.play()
-        }
-        else {}
-      }
-    };
-
-    useEffect(()=>{document.addEventListener("keydown", detectKeyDown, true)}, [])
-   */
 
   return (
-    <div className="App" onKeyDown={counter.handleKeyPress}>    
+    <div className="App">    
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Drum Machine</h1> 
-        <Counter />
+        <Counters />
       </header>
     </div>
   );
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,93 +1,96 @@
 import React from "react";
-//import styles from './Counter.module.css';
+import {useSelector, useDispatch} from "react-redux";
+import {handleChange, handleChangeName} from "./counterSlice";
+import styles from './Counter.module.css';
+import Badge from 'react-bootstrap/Badge';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
-export class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      padSounds: {
-        Q: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Yamaha%20DX100/22[kb]dx100-Pink-noise-C3a.wav.mp3",
-        W: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Yamaha%20DX100/65[kb]dx100-TomToms-C3.wav.mp3",
-        E: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Roland%20SH09/44[kb]stompbass1.wav.mp3",
-        A: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Pearl%20SC-40%20Kicks/65[kb]SC40BD028.wav.mp3",
-        S: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Pearl%20SC-40%20Kicks/35[kb]SC40BD07.wav.mp3",
-        D: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/34[kb]linn-BDRUM13.wav.mp3",
-        Z: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/15[kb]linn-Clap-1.wav.mp3",
-        X: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/14[kb]linn-COWB-v2.wav.mp3",
-        C: "https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/DRUM%20MACHINES/Linn%20Drums/122[kb]linn-RIDE-v2.wav.mp3",
-      },
-      input: "",
-    };
-    this.keys = this.arrayKeys(this.state.padSounds);
-    this.handleChange = this.handleChange.bind(this);
-  }
+export const Counters = () => {
+  const drumKits = useSelector((state) => state.counter.drumKits)
+  const nameDrumPart = useSelector((state) => state.counter.nameDrumPart)
+  const keyDrumPart = useSelector((state) => state.counter.keyDrumPart)
 
-  
-  arrayKeys(object) {
-    return Object.keys(object);
-  }
+  const dispatch = useDispatch();
 
-   handleChange(e) {
-    this.setState({
-      input: e
-    });
+  const drumKitsArray = Object.keys(drumKits)
 
-  }
 
-  handleKeyPress = (event) => {
-    let letters = this.keys
-    for (let letra of letters) {
-      if(event.key.toUpperCase() === letra){
-        console.log(letra+' press here! ')
-        document.getElementById(letra).play();
-      }
-  }
-  }
+  const drumKitsArrayMap = drumKitsArray.map((drumpart) =>{
+    let idx = React.createRef();
 
-  render() {
-    const drumpad = this.keys.map((one) => {
-      let idx = React.createRef();
-
-      function sound() {
-        idx.current.play();
-      }
-
-      return (
-        <span key={one} onClick={() => this.handleChange(one.toString())}>
-          <button
-            onClick={sound} 
-            id={one + this.keys.indexOf(one)}
-            style={{
-              backgroundColor: "whitegrey",
-              margin: 5,
-              width: 50,
-              height: 50,
-              border: "4px solid black",
-            }}
-            className="drum-pad"
-          >
-            {one}
-            <audio
-              id={one}
-              ref={idx}
-              className="clip"
-              src={this.state.padSounds[one]}
-            ></audio>
-          </button>
-        </span>
-      );
-    });
-
+    function sound() {
+      idx.current.play();
+    }
 
     return (
-      <div className="container-fluid" id="drum-machine">
-        <div id="display">
-          <h2>{
-           this.state.input
-          }</h2>
-          {drumpad}
-        </div>
-      </div>
+      <span key={drumpart} onClick={()=>{
+      dispatch(handleChange(drumpart));
+      dispatch(handleChangeName(drumKits[drumpart].name))
+      }}>
+        <button
+          onClick={sound} 
+          id={drumpart + drumKitsArray.indexOf(drumpart)}
+          style={{
+            backgroundColor: "whitegrey",
+            margin: 5,
+            width: 50,
+            height: 50,
+            border: "4px solid black",
+          }}
+          className="drum-pad"
+        >
+          {drumpart}
+          <audio
+            id={drumpart}
+            ref={idx}
+            className="clip"
+            src={drumKits[drumpart].sound}
+          ></audio>
+        </button>
+      </span>
     );
+  })
+
+  function handleKeyPress(event){
+    let letters = drumKitsArray
+    for (let letra of letters) {
+      if(event.key.toUpperCase() === letra){
+        console.log(letra+' press REDUX LETRA here! ')
+        document.getElementById(letra).play();
+       dispatch(handleChange(letra));
+        dispatch(handleChangeName(drumKits[letra].name))
+      }
   }
+  }
+
+  return (
+    <section className="container-fluid" id="drum-machine" onKeyDown={handleKeyPress}> 
+      <div id="display">
+   
+  
+   <Card className="text-center">
+      <Card.Header><h3>{`Key ${keyDrumPart} :`}<Badge bg="success">{nameDrumPart}</Badge></h3>
+</Card.Header>
+      <Card.Body>
+        <Button variant="secondary">   <span>{drumKitsArrayMap}</span>
+</Button>
+      </Card.Body>
+      <Card.Footer className="text-muted"> <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">React & Redux App</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="https://porfolioeduardojsilva.web.app/">Edu J. Silva</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar></Card.Footer>
+    </Card>
+    </div>
+</section>
+  )
 }
